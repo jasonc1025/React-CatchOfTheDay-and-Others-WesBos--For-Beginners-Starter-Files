32c03f05648cbbdd071244d58f24203fcb207a0e
jasonc1025 <jasonc@eande.world>
2018-04-08 22:25:13 -0700

    Customized FireBase

--- stepped-solutions/Finished App/src/base.js
+++ stepped-solutions/Finished App/src/base.js
@@ -2,9 +2,9 @@ import Rebase from "re-base";
 import firebase from "firebase";
 
 const firebaseApp = firebase.initializeApp({
-  apiKey: "AIzaSyDXUCNSpi5u07F76httlCTXAA4mPVQlEHs",
-  authDomain: "catch-of-the-day-wes-bos-2.firebaseapp.com",
-  databaseURL: "https://catch-of-the-day-wes-bos-2.firebaseio.com"
+  apiKey: "AIzaSyB5de25xoSuB7wlAuVciokwCeTxWx4QFW4",
+  authDomain: "react-catchoftheday-wesbos.firebaseapp.com",
+  databaseURL: "https://react-catchoftheday-wesbos.firebaseio.com",
 });
 
 const base = Rebase.createClass(firebaseApp.database());
